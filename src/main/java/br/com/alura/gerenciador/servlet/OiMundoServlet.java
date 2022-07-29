b90d2a462664af6a28f210355940cae31db9c162
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-29 11:58:55 -0300

    Adição de comentários de forma didática

--- src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -1,7 +1,22 @@
 package br.com.alura.gerenciador.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class OiMundoServlet extends HttpServlet {
+@WebServlet(urlPatterns ="/oi") //definir o nome da url do Servlet
+public class OiMundoServlet extends HttpServlet { //para transformar a classe em Servlet
 
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		//definir o fluxo binário
+		PrintWriter out = resp.getWriter(); //desenvolver o conteúdo html
+		
+	}
 }
