49e3660ef0b5b7a93002ff32404e0e3212137094
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-30 20:31:31 -0300

    Exclusão de comentários

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -12,11 +12,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
