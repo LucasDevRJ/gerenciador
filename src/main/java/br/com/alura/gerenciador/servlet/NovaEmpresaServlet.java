4c23ff0a20e8d6034d7b145b68a5b35e58c9a703
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-31 22:05:02 -0300

    Alteração do método service para doPost

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -18,7 +18,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	//método do lado do servidor
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa!");
 		String nomeEmpresa = request.getParameter("nome");
 		PrintWriter out = response.getWriter();
