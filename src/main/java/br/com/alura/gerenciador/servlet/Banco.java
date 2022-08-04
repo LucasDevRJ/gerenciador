77d062800928e1e8821a78e40d9bcad0870b2351
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-03 21:20:54 -0300

    Criação da lista de Empresas

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,6 +1,11 @@
 package br.com.alura.gerenciador.servlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<>();
 
 	public void adiciona(Empresa empresa) {
 		
