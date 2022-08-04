f397ce120f2e7a5cf9cd660c8614d0000d5d2bb2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-03 21:04:49 -0300

    Criação dos métodos Getters e Setters

--- src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -4,4 +4,20 @@ public class Empresa {
 
 	private int id;
 	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
