4d45cf6296c1cf03545ab677aa345b4badc3e86c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-07 17:29:52 -0300

    Escrever no html

--- src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -1,6 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +18,8 @@ public class ListaEmpresasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Banco banco = new Banco();
+		List<Empresa> lista = banco.getEmpresas();	
+		PrintWriter out = response.getWriter();
 	}
 }
