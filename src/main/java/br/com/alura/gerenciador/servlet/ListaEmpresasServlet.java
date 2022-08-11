6c4e0ee7cd9eafc5bba16068a664f7cc2cb35d93
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-11 19:23:32 -0300

    Encaminhando o Java com o JSP

--- src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +21,9 @@ public class ListaEmpresasServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();	
+		request.setAttribute("empresas", lista);
 		
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);;
 	}
 }
