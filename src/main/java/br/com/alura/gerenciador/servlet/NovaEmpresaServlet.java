c25cba21e3dda3c2bb527d0659c56e6d3250ac4c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-13 17:35:11 -0300

    Conversão de Data para String

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,24 +17,35 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 
-	//método do lado do servidor
+	// método do lado do servidor
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa!");
-		
+
 		String nomeEmpresa = request.getParameter("nome");
 		String dataEmpresa = request.getParameter("data");
 		
+		Date dataAbertura = null;
+
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(dataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
-		
+		empresa.setDataAbertura(dataAbertura);
+
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
-		
-		RequestDispatcher rd =  request.getRequestDispatcher("/novaEmpresaCriada.jsp"); //enviar a requisição para esta página jsp
-		request.setAttribute("empresa", empresa.getNome()); //seta um atributo com um "apelido" a ele 
-		rd.forward(request, response); //ativa o percurso da requisição
+
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp"); // enviar a requisição para esta
+																						// página jsp
+		request.setAttribute("empresa", empresa.getNome()); // seta um atributo com um "apelido" a ele
+		rd.forward(request, response); // ativa o percurso da requisição
 	}
 }
